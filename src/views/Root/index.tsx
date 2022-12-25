2e29dd37ad476af9354cf71b383faaafaaae85a2
Bohdanov Mikhail <bohdanov.mikhail@gmail.com>
2022-12-26 00:24:42 +0200

    Added material UI
    How to do:
    https://mui.com/material-ui/getting-started/installation/

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -1,3 +1,11 @@
+import { Container } from '@mui/material';
+
+import { Header } from './components/Header';
+
 export function Root() {
-  return null;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
