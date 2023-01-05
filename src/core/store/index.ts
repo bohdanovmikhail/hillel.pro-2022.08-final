df44f1563a675251a6cee0b51eb35c5e9a34f3c2
Bohdanov Mikhail <bohdanov.mikhail@gmail.com>
2023-01-06 00:15:52 +0200

    Added redux logger

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,11 +1,15 @@
 import { applyMiddleware, compose, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
 import messagesMiddleware from './messages/middleware';
+import usersMiddleware from './users/middleware';
 
 const middlewares = [
+  createLogger({ collapsed: true }),
   messagesMiddleware,
+  usersMiddleware,
 ];
 
 export default createStore(rootReducer, compose(applyMiddleware(...middlewares)));
