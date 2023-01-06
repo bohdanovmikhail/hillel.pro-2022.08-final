8b4ac3101778b828d5d8cd61be727e98599d6774
Bohdanov Mikhail <bohdanov.mikhail@gmail.com>
2023-01-07 00:38:29 +0200

    Added factories for mock entities

---
+++ src/core/mock/createMockChat.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+
+import { ChatModel } from '@models';
+import { createMockMessage } from './createMockMessage';
+
+export function createMockChat(override?: Partial<ChatModel>): ChatModel {
+  return {
+    id: faker.datatype.uuid(),
+    title: faker.lorem.text(),
+    lastMessage: createMockMessage(),
+    ...override,
+  };
+}
